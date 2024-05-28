17dddc4c658b130cf715d28cb40d0ed4cf4cfc55
David Finster <david@fortc.com>
2024-05-28 17:58:48 -0400

    chore: Initial commit

---
+++ website/blog/2024-05-01/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: 'Hello World!'
+authors: [dfinster]
+---
+
+Hello, world!
---
+++ website/docusaurus.config.ts
@@ -0,0 +1,79 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'fortc',
+  tagline: 'Finster\'s Occasional Reservoir of Temporary Comments',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://fortc.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+  trailingSlash: false,
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  onBrokenAnchors: 'throw',
+  onDuplicateRoutes: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          showReadingTime: false,
+          routeBasePath: '/', // Serve the blog at the site's root
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: false,
+    },
+    // Replace with your project's social card
+    image: 'img/profile.jpg',
+    navbar: {
+      title: 'fortc',
+      // logo: {
+      //   alt: 'fortc',
+      //   src: 'img/fortc.png',
+      // },
+      items: [
+        {
+          href: 'https://github.com/dfinster',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+      ],
+      copyright: `Site contents &copy; ${new Date().getFullYear()} David Finster`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
